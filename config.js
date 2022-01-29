9e268d0335face625dcd788452074ef881272ae2
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-01-29 09:01:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283147787992']
+global.owner = ['6282160268332','6282128476388','0']
 
 global.APIs = {
   amel: 'https://melcanz.com',
@@ -10,14 +10,14 @@ global.APIKeys = {
   'https://melcanz.com': '2tQnyfX2'
 }
 
-global.packname = 'ZED'
-global.author = '©ZED〽'
-global.wm = '©ZED〽'
-global.eror = 'error'
-global.wait = 'wait🍃'
-global.benar = '✅'
-global.salah = '❌'
-global.dikit = 'dikit lagi pack'
+global.packname = ' '
+global.author = 'By Experience Bot ©'
+global.wm = '𝘌𝘹𝘱𝘦𝘳𝘪𝘦𝘯𝘤𝘦'
+global.eror = '*Error Banh..*'
+global.wait = '*_Sabar.._*'
+global.benar = '*Benar* ✅'
+global.salah = 'Salah ❌'
+global.dikit = '*dikit lagi..*'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
 
 let file = require.resolve(__filename)
